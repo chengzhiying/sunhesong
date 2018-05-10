e5da863a52092f7d142123eafb4cbd80c5b7331e
chengzhiying <1141407392@qq.com>
2018-05-10 09:06:38 +0800

    孙贺淞是个傻逼

--- git_hub/src/main/java/com/example/git_hub/MainActivity.java
+++ git_hub/src/main/java/com/example/git_hub/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ///孙贺淞是个傻叉
     }
 }
